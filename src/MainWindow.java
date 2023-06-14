b57813f5b6bb432686e1f2865e45b7662b50a27d
Niki <nklasakk@gmail.com>
2023-06-14 16:31:13 +0200

    simplified label setting, renamed buttons

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -4,8 +4,8 @@ import java.awt.*;
 public class MainWindow {
     private JPanel panel;
     private JFrame frame;
-    private JButton button1;
-    private JButton button2;
+    private JButton buttonCheckAnswers;
+    private JButton buttonNewAddress;
     private JTextField networkAddressField;
     private JLabel ipLabel;
     private JLabel maskLabel;
@@ -14,10 +14,11 @@ public class MainWindow {
     private JTextField firstUsableField;
     private JTextField lastUsableField;
     private JTextField broadcastField;
-    private JButton button3;
+    private JButton buttonShowAnswers;
     private JLabel firstUsableResultLabel;
     private JLabel lastUsableResultLabel;
     private JLabel broadcastResultLabel;
+
     private IPAddress ipAddress;
     private SubnetMasks mask;
     private ResultCalculator result;
@@ -29,43 +30,19 @@ public class MainWindow {
         this.ipLabel.setText("IP: "+ ipAddress.toString());
         this.maskLabel.setText("Mask: "+mask.toString() + "/" + mask.getPrefix());
 
-
-        this.button1.addActionListener(e -> {
+        this.buttonCheckAnswers.addActionListener(e -> {
             String networkAddress = this.networkAddressField.getText();
             String firstUsable = this.firstUsableField.getText();
             String lastUsable = this.lastUsableField.getText();
             String broadcast = this.broadcastField.getText();
-            if (this.result.compareNetworkAddress(networkAddress)) {
-                this.networkAddressResultLabel.setForeground(Color.GREEN);
-                this.networkAddressResultLabel.setText("Network address: OK");
-            } else {
-                this.networkAddressResultLabel.setForeground(Color.RED);
-                this.networkAddressResultLabel.setText("Network address: BAD");
-            }
-            if (this.result.compareFirstHost(firstUsable)) {
-                this.firstUsableResultLabel.setForeground(Color.GREEN);
-                this.firstUsableResultLabel.setText("First usable address: OK");
-            } else {
-                this.firstUsableResultLabel.setForeground(Color.RED);
-                this.firstUsableResultLabel.setText("First usable address: BAD");
-            }
-            if (this.result.compareLastHost(lastUsable)) {
-                this.lastUsableResultLabel.setForeground(Color.GREEN);
-                this.lastUsableResultLabel.setText("Last usable address: OK");
-            } else {
-                this.lastUsableResultLabel.setForeground(Color.RED);
-                this.lastUsableResultLabel.setText("Last usable address: BAD");
-            }
-            if (this.result.compareBroadcastAddress(broadcast)) {
-                this.broadcastResultLabel.setForeground(Color.GREEN);
-                this.broadcastResultLabel.setText("Broadcast address: OK");
-            } else {
-                this.broadcastResultLabel.setForeground(Color.RED);
-                this.broadcastResultLabel.setText("Broadcast address: BAD");
-            }
+
+            compareAndSetResultLabel(networkAddressResultLabel, result.compareNetworkAddress(networkAddress), "Network address");
+            compareAndSetResultLabel(firstUsableResultLabel, result.compareFirstHost(firstUsable), "First usable address");
+            compareAndSetResultLabel(lastUsableResultLabel, result.compareLastHost(lastUsable), "Last usable address");
+            compareAndSetResultLabel(broadcastResultLabel, result.compareBroadcastAddress(broadcast), "Broadcast address");
         });
 
-        this.button2.addActionListener(e -> {
+        this.buttonNewAddress.addActionListener(e -> {
             this.ipAddress.generateNewAddress();
             this.mask = SubnetMasks.values()[(int) (Math.random() * 22)];
             this.result = new ResultCalculator(ipAddress.getIPAddress(), mask);
@@ -80,7 +57,8 @@ public class MainWindow {
             this.broadcastField.setText("");
             this.broadcastResultLabel.setText("");
         });
-        this.button3.addActionListener(e -> {
+
+        this.buttonShowAnswers.addActionListener(e -> {
             this.networkAddressField.setText(this.result.getNetworkAddressString());
             this.firstUsableField.setText(this.result.getFirstHostString());
             this.lastUsableField.setText(this.result.getLastHostString());
@@ -93,5 +71,18 @@ public class MainWindow {
         this.frame.setResizable(false);
         this.frame.pack();
         this.frame.setVisible(true);
+    }
+
+    private static void compareAndSetResultLabel(JLabel resultLabel, boolean isMatch, String labelName) {
+        if (isMatch) {
+            setResultLabel(resultLabel, Color.GREEN, labelName + ": OK");
+        } else {
+            setResultLabel(resultLabel, Color.RED, labelName + ": BAD");
+        }
+    }
+
+    private static void setResultLabel(JLabel resultLabel, Color color, String text) {
+        resultLabel.setForeground(color);
+        resultLabel.setText(text);
     }
 }
